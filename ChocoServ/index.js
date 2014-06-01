2b2c6dc85ddbba7905cf96f0dabd6d7311bca4f7
OpenShift guest <ryan@chocoserv.com>
2014-05-31 20:12:28 -0400

    DAMNSTRAIGHT

--- ChocoServ/index.js
+++ ChocoServ/index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('FUCK your "Hello World!"')
+  response.send('FUCK your "DAMN STRAIGHT!"')
 })
 
 app.listen(app.get('port'), function() {
